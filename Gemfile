ed3955f1981147307beb4dab76d227509ef53ffb
Michael Alt <michael@michael-Latitude-E6400.(none)>
2012-06-24 18:51:06 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+gem 'rails', '3.2.6'
+gem 'pg'
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+gem 'jquery-rails'
+gem "rspec-rails", ">= 2.10.1", :group => [:development, :test]
+gem "factory_girl_rails", ">= 3.3.0", :group => [:development, :test]
+gem "email_spec", ">= 1.2.1", :group => :test
+gem "devise", ">= 2.1.0"
+gem "cancan", ">= 1.6.7"
+gem "rolify", ">= 3.1.0"
+gem "bootstrap-sass", ">= 2.0.3"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harvestgeek::Application.config.secret_token = 'a30f529c362b25aef9c948c04477b67f0d6da645d050fb9ca5d8e0808c227de0e27c3f7c0b7e18f5426d7be37c19e0c5ed4b6be29f2f368a68491e9223cb5b34'
